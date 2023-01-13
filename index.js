1dd6a5a0eb017c6ec49e48676f217346ab7d8858
Josephine Opar <josephineopar@gmail.com>
2023-01-14 01:46:12 +0200

    Done with assignment

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
-// Code your solution in this file!
+let drivers = ['a', 'b', 'c', 'd', 'e', 'f']
+
+function returnFirstTwoDrivers(nameOfDrivers) {
+    return nameOfDrivers.slice(0, 2)
+}
+
+function returnLastTwoDrivers(nameOfDrivers) {
+    return nameOfDrivers.slice(-2)
+}
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers]
+
+function createFareMultiplier(num) {
+    return function yego(fare) { return fare * num }
+}
+
+function fareDoubler(num) {
+    return 2 * num
+}
+
+function fareTripler(num) {
+    return 3 * num
+}
+
+function selectDifferentDrivers(arrayOfDrivers, aFunction) {
+    if (aFunction === returnFirstTwoDrivers) {
+        return returnFirstTwoDrivers(arrayOfDrivers)
+    } else if (aFunction === returnLastTwoDrivers) {
+        return returnLastTwoDrivers(arrayOfDrivers)
+    }
+}
